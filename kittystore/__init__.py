068c7e1f894d24186bc78ed519b546bfe4cc645e
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-04-25 23:45:17 +0200

    Create the interface/API to be implemented by each store to retrieve the email from the backend

---
+++ kittystore/__init__.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+"""
+MMEmail - Interface defining the API to be implemented for each store
+          according to their specific back-end
+
+Copyright (C) 2012 Pierre-Yves Chibon
+Author: Pierre-Yves Chibon <pingou@pingoured.fr>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+See http://www.gnu.org/copyleft/gpl.html  for the full text of the
+license.
+"""
+
+
+import abc
+
+
+class KittyStore(object):
+    """ Interface to query emails from the database. """
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __init__(self, url, debug=False):
+        """ Constructor.
+        Create the session using the engine defined in the url.
+
+        :arg url, information necessary to assure the connection to the
+        database.
+        :kwarg debug, a boolean to set the debug mode on or off.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_archives(self, list_name, start, end):
+        """ Return all the thread started emails between two given dates.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg start, a datetime object representing the starting date of
+        the interval to query.
+        :arg end, a datetime object representing the ending date of
+        the interval to query.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_archives_length(self, list_name):
+        """ Return a dictionnary of years, months for which there are
+        potentially archives available for a given list (based on the
+        oldest post on the list).
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_email(self, list_name, message_id):
+        """ Return an Email object found in the database corresponding
+        to the Message-ID provided.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg message_id, Message-ID as found in the headers of the email.
+        Used here to uniquely identify the email present in the database.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_list_size(self, list_name):
+        """ Return the number of emails stored for a given mailing list.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_thread_length(self, list_name, thread_id):
+        """ Return the number of email present in a thread. This thread
+        is uniquely identified by its thread_id.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg thread_id, unique identifier of the thread as specified in
+        the database.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_thread_participants(self, list_name, thread_id):
+        """ Return the list of participant in a thread. This thread
+        is uniquely identified by its thread_id.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg thread_id, unique identifier of the thread as specified in
+        the database.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def search_content(self, list_name, keyword):
+        """ Returns a list of email containing the specified keyword in
+        their content.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg keyword, keyword to search in the content of the emails.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def search_content_subject(self, list_name, keyword):
+        """ Returns a list of email containing the specified keyword in
+        their content or their subject.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg keyword, keyword to search in the content or subject of
+        the emails.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def search_sender(self, list_name, keyword):
+        """ Returns a list of email containing the specified keyword in
+        the name or email address of the sender of the email.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg keyword, keyword to search in the database.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def search_subject(self, list_name, keyword):
+        """ Returns a list of email containing the specified keyword in
+        their subject.
+
+        :arg list_name, name of the mailing list in which this email
+        should be searched.
+        :arg keyword, keyword to search in the subject of the emails.
+        """
+        raise NotImplementedError

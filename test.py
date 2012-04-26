84bae17582654096613764e7552b92c1fcf46796
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-04-26 20:54:17 +0200

    Adjust the test file to the new API

--- test.py
+++ test.py
@@ -5,22 +5,24 @@ from kittystore.kittysastore import KittySAStore
 
 if __name__ == '__main__':
     URL = 'postgres://mm3:mm3@localhost/mm3'
-    #create(URL)
+    TABLE = 'email'
     STORE = KittySAStore(URL)#, debug=True)
-    print STORE.get_email('devel',
-        'Pine.LNX.4.55.0307210822320.19648@verdande.oobleck.net')
+    #create(URL)
+    print STORE.get_email(TABLE,
+        '3D97B04F.7090405@terra.com.br')
+        #'Pine.LNX.4.55.0307210822320.19648@verdande.oobleck.net')
     START = datetime.datetime(2012, 3, 1)
     END = datetime.datetime(2012, 3, 30)
-    print len(STORE.get_archives('devel', START, END))
-    print STORE.get_thread_length('devel',
+    print len(STORE.get_archives(TABLE, START, END))
+    print STORE.get_thread_length(TABLE,
         '4FCWUV6BCP3A5PASNFX6L5JOAE4GJ7F2')
-    print STORE.get_thread_participants('devel',
+    print STORE.get_thread_participants(TABLE,
         '4FCWUV6BCP3A5PASNFX6L5JOAE4GJ7F2')
-    print STORE.get_archives_length('devel')
-    print 'Subject', len(STORE.search_subject('devel', 'rawhid'))
-    print 'Content', len(STORE.search_content('devel', 'rawhid'))
+    print STORE.get_archives_length(TABLE)
+    print 'Subject', len(STORE.search_subject(TABLE, 'rawhid'))
+    print 'Content', len(STORE.search_content(TABLE, 'rawhid'))
     print 'Content-Subject', len(
-        STORE.search_content_subject('devel', 'rawhid'))
-    print 'Sender', len(STORE.search_sender('devel', 'pingou'))
-    print STORE.get_list_size('devel')
+        STORE.search_content_subject(TABLE, 'rawhid'))
+    print 'Sender', len(STORE.search_sender(TABLE, 'pingou'))
+    print STORE.get_list_size(TABLE)
 
